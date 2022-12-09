46cd620cf77e137fc970288209a939550a97ad6d
aditi <sharmaaditi7505@gmail.com>
2022-12-09 16:19:36 +0530

    Create calender_my.py

---
+++ calender_my.py
@@ -0,0 +1,11 @@
+import calendar
+y=int(input("enter the year"))
+m=1
+print("\n*****CALENDAR*****")
+Cal=calendar.TextCalendar(calendar.MONDAY)
+i=1
+while i<=12:
+    Cal.prmonth(y,i)
+    print()
+    i+=1
+      
